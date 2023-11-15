1bd62992f4a1e685ffb13cb9e9c008db258d779e
alisha fazal <alisha_fazal@hotmail.com>
2023-11-15 16:05:31 +0000

    Add comments to describe the function of each method

--- hangman.py
+++ hangman.py
@@ -4,7 +4,9 @@ from google.oauth2.service_account import Credentials
 from hangman_images import HangmanImages
 
 class Hangman:
-
+    """
+    The Hangman class contains all game variables and methods needed to play the game
+    """
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
         "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
@@ -18,6 +20,12 @@ class Hangman:
         self.hangman_images = self.get_hangman_images(self.difficulty)
 
     def select_difficulty(self):
+        """
+        User input corresponds to the level of difficulty the user would like to play at
+        Try except statement is nested within a while loop and checks if the user has 
+        entered an integer, if not, an error message will be raised. The loop is broken
+        when valid input is entered. 
+        """
         print("Which level of difficulty would you like to play?")
         print("1 - Easy")
         print("2 - Medium")
@@ -27,7 +35,8 @@ class Hangman:
         valid_difficulty = False
         while valid_difficulty == False:
             try:
-                user_selected_difficulty = int(input("Enter the number of the coressponding level(1-4):\n"))
+                user_selected_difficulty = int(input
+                ("Enter the number of the coressponding level(1-4):\n"))
                 print()
                 valid_difficulty = True
             except ValueError as e:
@@ -50,6 +59,9 @@ class Hangman:
         return user_selected_difficulty
 
     def get_number_of_lives(self, difficulty):
+        """
+        Acesses number of lives based on level of difficulty
+        """
         access_lives = {"Easy": 6, "Medium": 5, "Hard": 4}
 
         if difficulty == 1:
@@ -66,6 +78,15 @@ class Hangman:
             return hard_lives
 
     def get_word(self, difficulty):
+        """
+        Code to use Google Sheets API to retrive the hangman words.
+        if/elif statements access the easy, medium and hard words columns
+        in the Google Sheet respectively, and returns a random word to
+        use in the game.
+        The code to access the spreadsheet data from lines 88-98 was
+        taken from Code Institute's "Love Sandwiches" walkthrough project
+        """
+
         SCOPE = [
             "https://www.googleapis.com/auth/spreadsheets",
             "https://www.googleapis.com/auth/drive.file",
@@ -95,6 +116,10 @@ class Hangman:
             return hard_random_word
 
     def get_hidden_word(self, chosen_word):
+        """
+        For the random word, each letter of the random word is iterated over using a
+        for loop, replaced with a dash and stored in a varible for use later in the game
+        """
         hidden_word = ""
         hidden_letter = "-"
         for letter in chosen_word:
@@ -102,6 +127,10 @@ class Hangman:
         return hidden_word
 
     def get_hangman_images(self, difficulty):
+        """
+        Gets the hangman images to use each time a life is lost, corresponding
+        to the level of difficulty chosen
+        """
         if difficulty == 1:
             return HangmanImages.easy_lives_images()
         elif difficulty == 2:
@@ -110,6 +139,11 @@ class Hangman:
             return HangmanImages.hard_lives_images()
 
     def make_a_guess(self):
+        """
+        Try except else statement is placed within a while loop. If user input
+        is not a letter of the alphabet, then a ValueError is raised. The loop
+        is broken when valid input is entered. The user input is stored in a variable
+        """
         correct_input = False
         while correct_input == False:
             try:
@@ -123,6 +157,11 @@ class Hangman:
         return user_guess
 
     def check_user_guess(self, user_guess, current_word_hidden):
+        """
+        Checks for a match between the user input and the letters within the chosen word.
+        a dash is removed and replaced with correct input and for incorrect input a life
+        is lost
+        """
         letter_match = False
         for index in range(len(self.chosen_word)):
             if self.chosen_word[index] == user_guess:
@@ -136,12 +175,22 @@ class Hangman:
         return current_word_hidden
 
     def has_word_been_guessed(self, current_word_hidden):
+        """
+        For loop iterates over each letter in the current word to determine
+        if any dashes remain which determines if the word has been 
+        guessed or not
+        """
         for letter in current_word_hidden:
             if letter == "-":
                 return False
         return True
 
     def restart_game(self):
+        """
+        Restarts the game if user inputs y, program stops if user inputs n.
+        While loop checks whether the input is valid or not and is broken
+        when valid input is given
+        """
         valid_answer = False
         while valid_answer == False:
             play_again = input("Would you like to play again?\ny/n \n")
@@ -161,6 +210,11 @@ class Hangman:
 
 
     def run_game(self):
+        """
+        Runs the main game play. If statement is used to check whether the user has
+        won or lost based on whether the word has been guessed or if there are no
+        lives remaining
+        """
         print("Let's play!\n")
         while self.number_of_lives > 0:
             user_guess = self.make_a_guess()
