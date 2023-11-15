cd16a83111add813344aa726c1555e2a01feb0c8
alisha fazal <alisha_fazal@hotmail.com>
2023-11-15 23:39:48 +0000

    Add code comments, fix all errors raised through the CI Python linter

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
+# Importing the Hangman class from the hangman file
 from hangman import Hangman
 
+"""
+Below an instance of the Hangman class is created
+and the run_game method is called and ran
+"""
 hangman_game = Hangman()
 hangman_game.run_game()
